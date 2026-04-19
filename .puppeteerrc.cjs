f2078118c8c145f81f901a17347165e2a40786f8
SKARABULUT91 <soner91ksk@gmail.com>
2026-04-19 20:30:30 +0300

    Update .puppeteerrc.cjs

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,10 +1,11 @@
-const {join} = require('path');
+const { join } = require('path');
 
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // Bu satır Puppeteer'a kurulumu nereye yapacağını ve 
-  // NEREDEN okuyacağını zorla söyler.
+  // Puppeteer'a Chrome'u tam olarak nereye kuracağını ve 
+  // çalışma anında nereden okuyacağını dikte eder.
+  // Bu yöntemle Render üzerindeki "Permission Denied" hataları baypas edilir.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
